a744f1661829e829fcefbdd7f8693b815464eb2d
Drew Lentz <drew@codergv.org>
2024-10-23 08:59:55 -0500

    Update config.js
    HEATMAP_RADIUS: 10, from 20

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {    
     MAP_CENTER: [26.2034, -98.2300],
     MAP_ZOOM: 12,
-    HEATMAP_RADIUS: 20,
+    HEATMAP_RADIUS: 10,
     HEATMAP_BLUR: 15,
     HEATMAP_MAX_ZOOM: 14,
     NEARBY_RADIUS: 1000 // 1km
