0772cc13e37a618b70d9cbce93dc9e3e7a2d5c7f
Drew Lentz <drew@codergv.org>
2024-10-23 09:14:50 -0500

    Update map.js
    R9

--- map.js
+++ map.js
@@ -42,8 +42,8 @@ function initMap() {
         blur: config.HEATMAP_BLUR,
         maxZoom: config.HEATMAP_MAX_ZOOM,
         max: 1.0,
-        minOpacity: 0.4,
-        maxOpacity: 0.1,  // Adjust this value between 0 and 1 (default is 0.6)
+        minOpacity: 0.1,
+        maxOpacity: 0.6,  // Adjust this value between 0 and 1 (default is 0.6)
     });
 
     map.on('zoomend', updateMapView);
