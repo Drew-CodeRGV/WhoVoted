6f51897ade29877d54442dd513a5dac7536c4420
Drew Lentz <drew@codergv.org>
2024-10-23 12:14:21 -0500

    Update map.js
    R13

--- map.js
+++ map.js
@@ -6,7 +6,7 @@ function initMap() {
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 18,
-        attribution: '© OpenStreetMap contributors | DL-R12'
+        attribution: '© OpenStreetMap contributors | DL-R13'
     }).addTo(map);
 
     markerClusterGroup = L.markerClusterGroup({
