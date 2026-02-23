63b692bab9e0de49b2c3aab35393d37776bdc5e4
Drew CodeRGV <drew@codergv.org>
2026-02-22 21:34:13 -0600

    Move geolocation to standalone icon, scale all bottom-left icons for mobile

--- public/search.js
+++ public/search.js
@@ -1,6 +1,14 @@
 // Search functionality using Nominatim - Bottom-left icon + popup
 let searchTimeout = null;
 
+// Create geolocation icon button (standalone, next to info and search icons)
+const geoIconBtn = document.createElement('button');
+geoIconBtn.className = 'geo-icon-btn';
+geoIconBtn.id = 'geolocation-button';
+geoIconBtn.title = 'Find my location';
+geoIconBtn.innerHTML = '<i class="fas fa-crosshairs"></i>';
+document.body.appendChild(geoIconBtn);
+
 // Create search icon button
 const searchIconBtn = document.createElement('button');
 searchIconBtn.className = 'search-icon-btn';
@@ -15,11 +23,6 @@ searchPopup.innerHTML = `
     <div class="search-popup-inner">
         <input id="search-input" type="text" placeholder="Search for an address">
         <button class="search-popup-btn" id="search-button">Search</button>
-        <button class="search-popup-geo" id="geolocation-button" title="Find my location">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="20" height="20">
-                <path d="M12 8c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm8.94 3c-.46-4.17-3.77-7.48-7.94-7.94V1h-2v2.06C6.83 3.52 3.52 6.83 3.06 11H1v2h2.06c.46 4.17 3.77 7.48 7.94 7.94V23h2v-2.06c4.17-.46 7.48-3.77 7.94-7.94H23v-2h-2.06zM12 19c-3.87 0-7-3.13-7-7s3.13-7 7-7 7 3.13 7 7-3.13 7-7 7z"/>
-            </svg>
-        </button>
     </div>
 `;
 document.body.appendChild(searchPopup);
