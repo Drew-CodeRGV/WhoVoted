edd175bb4e8ac6b675940db42364c8aea5b556a2
Drew CodeRGV <drew@codergv.org>
2026-02-22 21:25:52 -0600

    Set heatmap threshold to zoom 17

--- public/config.js
+++ public/config.js
@@ -3,7 +3,7 @@ const config = {
   MAP_ZOOM: 10,
   HEATMAP_RADIUS: 15,
   HEATMAP_BLUR: 15,
-  HEATMAP_MAX_ZOOM: 15,
+  HEATMAP_MAX_ZOOM: 17,
   NEARBY_RADIUS: 1000, // 1km
   NOMINATIM_ENDPOINT: 'https://nominatim.openstreetmap.org',
   USER_AGENT: 'WhoVoted/2.0 (civic voter turnout mapping)'
